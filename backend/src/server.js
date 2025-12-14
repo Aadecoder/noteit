802e77d78fd9906b1e634481cc4cebddbac52d75
Aadecoder <adityandr8274@gmail.com>
2025-12-15 03:52:47 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
@@ -9,12 +10,15 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 
 app.use(express.json()); // This middleware will parse the JSON Bodies: req.body
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 // another simple custom middleware
 app.use((req, res, next)=>{
     console.log(`Req method is ${req.method} and Req URL is ${req.url}`);
@@ -23,6 +27,16 @@ app.use((req, res, next)=>{
 
 
 app.use("/api/notes", notesRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
